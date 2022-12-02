e0cafe5be23be8df5f66751d51f2e1ff0b2ee5e1
Me(Laptop) <Roman.Bikhtiaiev@protonmail.ch>
2022-12-02 19:53:36 +0200

    [cxx] add atomic sync example

---
+++ atomic_flags/atomic_sync.cpp
@@ -0,0 +1,47 @@
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <iostream>
+
+std::vector<int> vec;
+std::atomic_flag flag{};
+
+void prepare_work() {
+	vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	std::cout << "Preparation complete" << std::endl;
+	flag.test_and_set();
+	flag.notify_all();
+}
+
+void add_work() {
+	std::cout << "Add worker" << std::endl;
+	flag.wait(false);
+	int accum = 0;
+	for (auto i : vec) {
+		accum += i;
+	}
+	std::cout << "Add worker completed. Sum = " << accum << std::endl;
+}
+
+void sub_work() {
+	std::cout << "Sub worker" << std::endl;
+	flag.wait(false);
+	int accum = 0;
+	for (auto i : vec) {
+		accum -= i;
+	}
+	std::cout << "Sub worker completed. Sub = " << accum << std::endl;
+}
+
+int main() {
+	std::cout << "Staring..." << std::endl;
+
+	std::thread preparation(prepare_work);
+	std::thread add(add_work);
+	std::thread sub(sub_work);
+
+	preparation.join();
+	add.join();
+
+	std::cout << "End..." << std::endl;
+}
